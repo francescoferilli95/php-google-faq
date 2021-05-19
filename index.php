b65177c12d3b7130483361fe3419bcc076312fac
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-19 14:48:42 +0200

    creating repository & adding header plus styles

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FONT FAMILY -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <!-- FONTAWESOME -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css' integrity='sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==' crossorigin='anonymous'/>
+    <!-- STYLES -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <header>
+        <div class="left">
+            <a href="/"><img class="img" src="https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png" alt="Google Logo"></a>
+            <a href="/"><span class="text">Privacy e termini</span></a>
+            <ul class="list">
+                <li> <a href="#">Introduzione</a></li>
+                <li> <a href="#">Norme sulla privacy</a></li>
+                <li> <a href="#">Termini di servizio</a></li>
+                <li><a href="#">Tecnologie</a></li>
+                <li><a href="#">Domande frequenti</a></li>
+            </ul>
+        </div>
+        <div class="right">
+            <div class="menu"><i class="fas fa-bars"></i></div>
+            <div class="account">F</div>
+        </div>
+    </header>
+
+    <main class="container">
+
+    </main>
+</body>
+</html>
